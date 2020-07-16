bae78c85ca1ecbbdbb26e3edbe5e18f3718c4c42
Valeria Luque <valeriapluque@gmail.com>
2020-07-15 22:19:54 -0300

    docs agregar

---
+++ docs/precache-manifest.126aead2e3cf1a1564497484e2c13067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af303186f41f766001b988e50f7f291a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1a469d17ec683cc91c2",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0b96d6ca684ddf638358",
+    "url": "/static/js/2.3542fd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3542fd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a469d17ec683cc91c2",
+    "url": "/static/js/main.1b074f91.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
